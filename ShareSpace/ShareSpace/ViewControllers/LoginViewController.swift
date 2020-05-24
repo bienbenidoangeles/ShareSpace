c18799266225459cbedec618fa8eb5ff50571f1a
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-05-24 12:12:54 -0400

    added groups and login vc

---
+++ ShareSpace/ShareSpace/ViewControllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  ShareSpace
+//
+//  Created by Bienbenido Angeles on 5/24/20.
+//  Copyright © 2020 Bienbenido Angeles. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
