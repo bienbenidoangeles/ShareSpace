a52ca6a2e0505d4e47a4a40c20de0925f60b758d
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-05-26 19:17:03 -0400

    added storyboard wireframe

---
+++ Capstone wireframe/Capstone wireframe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capstone wireframe
+//
+//  Created by Bienbenido Angeles on 5/26/20.
+//  Copyright © 2020 Bienbenido Angeles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
