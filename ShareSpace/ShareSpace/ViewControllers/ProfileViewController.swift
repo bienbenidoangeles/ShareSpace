b4ae9e6cde1cb55a3a6d7c44b62e713e6a455d83
Yuliia Engman <yuliia.engman@gmail.com>
2020-05-28 11:55:16 -0400

    edit profile added functionality

--- ShareSpace/ShareSpace/ViewControllers/ProfileViewController.swift
+++ ShareSpace/ShareSpace/ViewControllers/ProfileViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Kingfisher
 
 class ProfileViewController: UIViewController {
     
@@ -17,14 +19,227 @@ class ProfileViewController: UIViewController {
         view = profileView
     }
     
+    private lazy var imagePickerController: UIImagePickerController = {
+        let ip = UIImagePickerController()
+        ip.delegate = self
+        return ip
+    }()
+    
+    private var selectedImage: UIImage? {
+        didSet {
+            profileView.profileImageView.image = selectedImage
+        }
+    }
+    
+    //???
+    private let storageService = StorageService()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
        view.backgroundColor = .systemGroupedBackground
+        
+        updateUI()
+        
+        profileView.updateProfileButton.isHidden = true
+        profileView.editProfileImageButton.isHidden = true
+        
+        profileView.userDisplayNameTextfield.isHidden = true
+        profileView.userDisplayNameTextfield.delegate = self
+        
+        profileView.userNameTextfield.isHidden = true
+        profileView.userNameTextfield.delegate = self
+        
+        profileView.userPhoneNumberTextfield.isHidden = true
+        profileView.userPhoneNumberTextfield.delegate = self
+        
+        profileView.userBioTextfield.isHidden = true
+        profileView.userBioTextfield.delegate = self
+        
+        profileView.userOccupationTextfield.isHidden = true
+        profileView.userOccupationTextfield.delegate = self
+        
+        profileView.userCreditcardTextfield.isHidden = true
+        profileView.userCreditcardTextfield.delegate = self
+        profileView.userCreditcardCVVNumberTextfield.isHidden = true
+        profileView.userCreditcardCVVNumberTextfield.delegate = self
+        profileView.userExpirationDateTextfield.isHidden = true
+        profileView.userExpirationDateTextfield.delegate = self
+        
+       // profileView.editProfileImageButton.addTarget(self, action: #selector(userImageEditButtonPressed), for: .touchUpInside)
+        
+        profileView.editProfileButton.addTarget(self, action: #selector(editUserProfileButtonPressed), for: .touchUpInside)
+
     }
     
+    private func updateUI() {
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+//        emailLabel.text = user.email
+//        displayUsernameTextField.text = user.displayName
+//        profileImageView.kf.setImage(with: user.photoURL)
+        
+        //I think we have to add photo to user model
+        //profileView.profileImageView.kf.setImage(with: user.photoURL)
+       
+       // profileView.userDisplayNameLabel.text = user.displayName
+       
+        // profileView.userNameLabel.text = user.firstName
+       // profileView.phoneNumberLabel.text = user.phoneNumber
+        
+        profileView.emailLabel.text = user.email
+        
+        //profileView.bioLabel.text = user.bio
+       // profileView.occupationLabel.text = user.work
+        //profileView.governmentIdLabel.text = user.govermentId
+       // profileView.paymentLabel.text = user.payment
+        
+        
+    }
+    
+    @objc func userImageEditButtonPressed() {
+        
+        let alertController = UIAlertController(title: "Choose Photo Option", message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default)
+        { [weak self] alertAction in
+            self?.imagePickerController.sourceType = .camera
+            self?.present(self!.imagePickerController, animated: true)
+        }
+        let phototLibararyAction = UIAlertAction(title: "Photo Libarary", style: .default)
+        { [weak self] alertAction in
+            self?.imagePickerController.sourceType = .photoLibrary
+            self?.present(self!.imagePickerController, animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alertController.addAction(cameraAction)
+        }
+        alertController.addAction(phototLibararyAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+        
+    }
+    
+    @objc func editUserProfileButtonPressed() {
+        
+        //profileView.emailLabel
+        
+        profileView.editProfileImageButton.isHidden = false
+        
+        profileView.userDisplayNameTextfield.isHidden = false
+        profileView.userDisplayNameLabel.isHidden = true
+        
+        profileView.userNameTextfield.isHidden = false
+        profileView.userNameLabel.isHidden = true
+        
+        profileView.userPhoneNumberTextfield.isHidden = false
+        profileView.phoneNumberLabel.isHidden = true
+        
+        profileView.userBioTextfield.isHidden = false
+        profileView.bioLabel.isHidden = true
+        
+        profileView.userOccupationTextfield.isHidden = false
+        profileView.occupationLabel.isHidden = true
+        
+        profileView.userCreditcardTextfield.isHidden = false
+        profileView.userCreditcardCVVNumberTextfield.isHidden = false
+        profileView.userExpirationDateTextfield.isHidden = false
+        profileView.paymentLabel.isHidden = true
+        
+        profileView.editProfileImageButton.addTarget(self, action: #selector(userImageEditButtonPressed), for: .touchUpInside)
+        
+        profileView.editProfileButton.isHidden = true
+        profileView.updateProfileButton.isHidden = false
+        
+       // profileView.updateProfileButton.addTarget(self, action: #selector(updateUserProfileButtonPressed), for: .touchUpInside)
+    }
+    
+    /*
+    @objc func updateUserProfileButtonPressed() {
+        
+        guard let displayName = profileView.userDisplayNameTextfield.text,
+            !displayName.isEmpty,
+          let userFullName = profileView.userNameTextfield.text,
+            !userFullName.isEmpty,
+          let userPhoneNumber = profileView.userPhoneNumberTextfield.text,
+            !userPhoneNumber.isEmpty,
+        let userBio = profileView.userBioTextfield.text,
+            !userBio.isEmpty,
+            let userOccupation = profileView.userOccupationTextfield.text, !userOccupation.isEmpty,
+            let userCardNumber = profileView.userCreditcardTextfield.text, !userCardNumber.isEmpty,
+            let userCardCVVNumber = profileView.userCreditcardCVVNumberTextfield.text,
+            !userCardCVVNumber.isEmpty,
+            let userCardExpDate = profileView.userExpirationDateTextfield.text, !userCardExpDate.isEmpty,
+            let selectedImage = selectedImage else {
+                print("missing field")
+                return
+        }
+        
+        guard let user = Auth.auth().currentUser else { return }
+        //resize image before uploading to Firebase
+        //let resizedImage = UIImage.resizeImage(originalImage: selectedImage, rect: profileView.profileImageView.bounds)
+        let resizedImage = UIImage.resizeImage(selectedImage)
+        
+        print("original image size: \(selectedImage.size)")
+        print("resized image size: \(resizedImage)")
+        
+        //TODO: call storageService.upload
+        //need to update to user userId ot itemId
+        
+       // Update this code:
+        storageservice.uploadPhoto(userId: user.uid, image: resizedImage) { [weak self] (result) in
+            // code here to add the photoURL to the user's photoURL
+            //     property then commit changes
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.showAlert(title: "Error uploading photo", message: "\(error.localizedDescription)")
+                }
+            case .success(let url):
+                let request = Auth.auth().currentUser?.createProfileChangeRequest()
+                request?.displayName = displayName
+                request?.photoURL = url
+                request?.commitChanges(completion: { [unowned self] (error) in
+                    if let error = error {
+                        //TODO: show alert
+                        //print("CommitCjanges error: \(error)")
+                        DispatchQueue.main.async {
+                            self?.showAlert(title: "Error updating profile", message: "Error changing profile: \(error.localizedDescription)")
+                        }
+                    } else {
+                        //print("profile successfully updated")
+                        DispatchQueue.main.async {
+                            self?.showAlert(title: "Profile Updated", message: "Profile successfully updated")
+                        }
+                    }
+                })
+            }
+        }
+        
+        
+        
+        profileView.updateProfileButton.isHidden = false
+        
+    }
 
+*/
+}
 
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        selectedImage = image
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
--- ShareSpace/ShareSpace/Views/ProfileView.swift
+++ ShareSpace/ShareSpace/Views/ProfileView.swift
@@ -15,8 +15,25 @@ class ProfileView: UIView {
         profileImageView.layer.borderWidth = 3
         profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
         profileImageView.layer.borderColor = UIColor.white.cgColor
-        
     }
+    
+    public lazy var editProfileButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Edit Profile", for: .normal)
+        button.setTitleColor(.systemOrange, for: .normal)
+        
+        return button
+    }()
+    
+    public lazy var updateProfileButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Update Profile", for: .normal)
+        button.setTitleColor(.systemOrange, for: .normal)
+        
+        return button
+    }()
 
      public lazy var profileImageView: UIImageView = {
             let imageView = UIImageView()
@@ -31,6 +48,18 @@ class ProfileView: UIView {
             return imageView
         }()
     
+    //private
+    public lazy var editProfileImageButton: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(systemName: "camera.circle"), for: .normal)
+        //button.setTitleColor(.systemOrange, for: .normal)
+        button.tintColor = .systemOrange
+        
+        return button
+    }()
+    
+    //Do we use this? - now yes
     public lazy var userDisplayNameLabel: UILabel = {
         let label = UILabel()
         label.text = "display name"
@@ -38,6 +67,14 @@ class ProfileView: UIView {
         label.textAlignment = .left
         return label
     }()
+    
+    public lazy var userDisplayNameTextfield: UITextField = {
+           let textfield = UITextField()
+           textfield.placeholder = "Enter username"
+           //textfield.numberOfLines = 1
+           textfield.textAlignment = .left
+           return textfield
+       }()
         
         public lazy var userNameLabel: UILabel = {
             let label = UILabel()
@@ -47,6 +84,13 @@ class ProfileView: UIView {
             return label
         }()
     
+    public lazy var userNameTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your first and last name"
+        textfield.textAlignment = .left
+        return textfield
+    }()
+    
     
     
     public lazy var phoneNumberLabel: UILabel = {
@@ -55,6 +99,13 @@ class ProfileView: UIView {
         label.numberOfLines = 1
         label.textAlignment = .left
         return label
+    }()
+    
+    public lazy var userPhoneNumberTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your phone number"
+        textfield.textAlignment = .left
+        return textfield
     }()
         
         public lazy var emailLabel: UILabel = {
@@ -68,9 +119,16 @@ class ProfileView: UIView {
     public lazy var bioLabel: UILabel = {
         let label = UILabel()
         label.text = "bio"
-        label.numberOfLines = 1
+        label.numberOfLines = 3
         label.textAlignment = .left
         return label
+    }()
+    
+    public lazy var userBioTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Type your bio"
+        textfield.textAlignment = .left
+        return textfield
     }()
     
     public lazy var occupationLabel: UILabel = {
@@ -79,6 +137,13 @@ class ProfileView: UIView {
         label.numberOfLines = 1
         label.textAlignment = .left
         return label
+    }()
+    
+    public lazy var userOccupationTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your occupation"
+        textfield.textAlignment = .left
+        return textfield
     }()
     
     public lazy var governmentIdLabel: UILabel = {
@@ -95,6 +160,27 @@ class ProfileView: UIView {
         label.numberOfLines = 1
         label.textAlignment = .left
         return label
+    }()
+    
+    public lazy var userCreditcardTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your credit ot debit card number"
+        textfield.textAlignment = .left
+        return textfield
+    }()
+    
+    public lazy var userCreditcardCVVNumberTextfield: UITextField = {
+           let textfield = UITextField()
+           textfield.placeholder = "Enter your card CVV number"
+           textfield.textAlignment = .left
+           return textfield
+       }()
+    
+    public lazy var userExpirationDateTextfield: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your card expiration date"
+        textfield.textAlignment = .left
+        return textfield
     }()
         
         
@@ -138,12 +224,44 @@ class ProfileView: UIView {
             }
             
             private func commomInit() {
+                setUpEditProfileButtonConstraints()
+                
+                setUpUpdateProfileButtonConstraints()
+                
                 setUpProfileImageViewConstraints()
+                
+                setUpEditProfileImageButtonConstraints()
                 
                 setUpStackViewConstraints()
                 
+                setUpStackViewTextFieldConstraints()
+                
             }
+    
+    private func setUpEditProfileButtonConstraints() {
+            addSubview(editProfileButton)
+            editProfileButton.translatesAutoresizingMaskIntoConstraints =  false
+            
+            NSLayoutConstraint.activate([
+            
+                editProfileButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+               editProfileButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            ])
+        }
+    
+    private func setUpUpdateProfileButtonConstraints() {
+        addSubview(updateProfileButton)
+        updateProfileButton.translatesAutoresizingMaskIntoConstraints =  false
         
+        NSLayoutConstraint.activate([
+        
+            updateProfileButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+           updateProfileButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+        ])
+    }
+    
+    
+    
         private func setUpProfileImageViewConstraints() {
             addSubview(profileImageView)
             profileImageView.translatesAutoresizingMaskIntoConstraints =  false
@@ -156,15 +274,25 @@ class ProfileView: UIView {
                 profileImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
                 profileImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
                 profileImageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.32)
-            
-            
             ])
         }
+    
+    private func setUpEditProfileImageButtonConstraints() {
+        addSubview(editProfileImageButton)
+        editProfileImageButton.translatesAutoresizingMaskIntoConstraints =  false
+        
+        NSLayoutConstraint.activate([
+        
+            editProfileImageButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 140),
+           editProfileImageButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -140),
+        ])
+    }
         
 
         
         private func setUpStackViewConstraints() {
             addSubview(stackView)
+            stackView.addArrangedSubview(userDisplayNameLabel)
             stackView.addArrangedSubview(userNameLabel)
             stackView.addArrangedSubview(phoneNumberLabel)
             stackView.addArrangedSubview(emailLabel)
@@ -173,7 +301,7 @@ class ProfileView: UIView {
             stackView.addArrangedSubview(governmentIdLabel)
             stackView.addArrangedSubview(uploadIdButton)
             stackView.addArrangedSubview(paymentLabel)
-            stackView.addArrangedSubview(addPaymentButton)
+           // stackView.addArrangedSubview(addPaymentButton)
             
             stackView.translatesAutoresizingMaskIntoConstraints =  false
             
@@ -182,10 +310,34 @@ class ProfileView: UIView {
                 stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
                 stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
                 stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
-               // stackView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.04)
-            
             ])
         }
+    
+    private func setUpStackViewTextFieldConstraints() {
+        addSubview(stackView)
+        stackView.addArrangedSubview(userDisplayNameTextfield)
+        stackView.addArrangedSubview(userNameTextfield)
+        stackView.addArrangedSubview(userPhoneNumberTextfield)
+        stackView.addArrangedSubview(emailLabel)
+        stackView.addArrangedSubview(userBioTextfield)
+        stackView.addArrangedSubview(userOccupationTextfield)
+        stackView.addArrangedSubview(governmentIdLabel)
+        stackView.addArrangedSubview(uploadIdButton)
+        stackView.addArrangedSubview(userCreditcardTextfield)
+        stackView.addArrangedSubview(userCreditcardTextfield)
+        stackView.addArrangedSubview(userCreditcardCVVNumberTextfield)
+        stackView.addArrangedSubview(userExpirationDateTextfield)
+       // stackView.addArrangedSubview(addPaymentButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints =  false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 50),
+            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
+        ])
+    }
         
         
        
