b724f339c59b3e75982831917b6ba73efefda811
Yuliia Engman <yuliia.engman@gmail.com>
2020-06-12 15:30:45 -0400

    added sigh out button on FirstProfileVC

--- ShareSpace/ShareSpace/ViewControllers/FirstProfileViewController.swift
+++ ShareSpace/ShareSpace/ViewControllers/FirstProfileViewController.swift
@@ -12,39 +12,55 @@ import FirebaseStorage
 
 class FirstProfileViewController: UIViewController {
     
-    
-    
-      private var user: UserModel?
+    private var user: UserModel?
     
     private let userId: String
     init?(coder: NSCoder, userId: String) {
-      self.userId = userId
-      super.init(coder: coder)
+        self.userId = userId
+        super.init(coder: coder)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    @IBOutlet var userImegeView: UIView!
+    
+    
+    @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var userTypeLabel: UILabel!
     @IBOutlet weak var userRating: UILabel!
     @IBOutlet weak var userPhoneNumber: UILabel!
-    
     @IBOutlet weak var spacesCollectionView: UICollectionView!
-    
     @IBOutlet weak var reviewsCollectionView: UICollectionView!
-    
-    
     @IBOutlet weak var userEmail: UILabel!
+    
+    
+    
+    private var selectedImage: UIImage? {
+        didSet {
+            userImage.image = selectedImage
+        }
+    }
+    
+    private let storageService = StorageService.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadUser()
-        
-        //updateUI()
     }
     
+    
+    @IBAction func signOutButtonPressed(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+            UIViewController.showViewController(viewcontroller: LoginViewController())
+        } catch {
+            DispatchQueue.main.async {
+                self.showAlert(title: "Unable to signout", message: "Error: \(error.localizedDescription)")
+            }
+        }
+    }
     
     func updateUI() {
         guard let user = user else {
@@ -52,10 +68,12 @@ class FirstProfileViewController: UIViewController {
         }
         if let user = Auth.auth().currentUser {
             if user.uid == self.userId {
+                //works
                 userEmail.text = user.email ?? "no email"
             }
         }
-      //  userEmail.text = user.userEmail
+        userNameLabel.text = user.displayName
+        userPhoneNumber.text = user.phoneNumber
     }
     
     func loadUser() {
@@ -72,11 +90,10 @@ class FirstProfileViewController: UIViewController {
     }
     
     @IBAction func editButton(_ sender: UIButton) {
-            guard let user = Auth.auth().currentUser else {
-                return
-            }
-          
-            let editProfilelVC = ProfileViewController()
-            navigationController?.pushViewController(editProfilelVC, animated: true)
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        let editProfilelVC = ProfileViewController()
+        navigationController?.pushViewController(editProfilelVC, animated: true)
     }
 }
